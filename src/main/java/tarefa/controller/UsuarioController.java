3f888e549f0558bc52a0a9e1938263a785782d9d
Márcio Velasco <Mrcio Velasco@NoteVelasco>
2017-07-22 09:22:11 -0300

    WebService usuario.

--- src/main/java/tarefa/controller/UsuarioController.java
+++ src/main/java/tarefa/controller/UsuarioController.java
@@ -1,15 +1,63 @@
 package tarefa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import repository.UsuarioBD;
+import tarefa.modelo.Usuario;
 
+@Controller
 @RequestMapping(path="/users")
 public class UsuarioController {
 	
 	@Autowired
 	private UsuarioBD usuarioBd;
 	
+	@GetMapping()
+	public @ResponseBody Iterable<Usuario> all(){
+		return usuarioBd.findAll();
+	}
+	
+	@RequestMapping(value="/salvar", method = RequestMethod.POST)
+	public @ResponseBody String salvarUsuario(Usuario usuario){
+		usuarioBd.save(usuario);
+		return "Saved";
+	}
+	
+	@PostMapping
+	public @ResponseBody String add(@RequestBody Usuario usuario){
+		usuarioBd.save(usuario);
+		return "Saved";
+	}
+	
+	@DeleteMapping(path="/{id}")
+	public @ResponseBody String delete(@PathVariable Long id){
+		usuarioBd.delete(id);
+		return "Deleted";
+	}
+	
+	@GetMapping(path="/{id}")
+	public ResponseEntity <Usuario> getOne(@PathVariable Long id){
+		Usuario usuario = usuarioBd.findOne(id);
+		return new ResponseEntity<Usuario>(usuario, HttpStatus.CREATED);
+	}
+	
+	@PutMapping(path="/{id}")
+	public ResponseEntity <Usuario> update(@PathVariable Integer id,
+									@RequestBody Usuario usuario){
+		usuarioBd.save(usuario);
+		return new ResponseEntity<Usuario>(usuario, HttpStatus.CREATED);
+	}
 	
 }
